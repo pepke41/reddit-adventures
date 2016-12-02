9149846fae1c6b2eff8bfb59faac2fb7d8ed3d44
Emily Pepke <elpepke41@gmail.com>
2016-12-02 01:52:18 -0500

    removes links when search bar is cleared

--- app/services/reddit.js
+++ app/services/reddit.js
@@ -1,10 +1,13 @@
 import Ember from 'ember';
 
 const { RSVP, $ } = Ember;
-const { Promise } = RSVP;
+const { Promise, resolve } = RSVP;
 
 export default Ember.Service.extend({
   getPosts(subreddit) {
+    if(!subreddit) {
+      return resolve([]);
+    }
     return new Promise(function(resolve) {
       $.ajax(`https://api.reddit.com/r/${subreddit}/new`).done(function(data) {
         var posts = data.data.children.map(function(child){
